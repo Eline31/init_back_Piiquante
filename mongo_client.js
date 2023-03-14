2ee2af4cb9f9e02a0013f74a99642ed7a384a3df
Eline31 <eline.chambon@gmail.com>
2023-03-14 16:54:51 +0100

    Init back piiquante

---
+++ mongo_client.js
@@ -0,0 +1,19 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.2ol13jn.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+module.exports = client;
+
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Utilisateur", userSchema);
