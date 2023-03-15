dc7edea4eae9c1a30bab6642890da56e029191b1
Eline31 <eline.chambon@gmail.com>
2023-03-15 16:48:14 +0100

    Connection serveur OK, suppression http

--- app.js
+++ app.js
@@ -1,52 +1,5 @@
-const express = require("express");
-const app = express();
+const app = require("./server");
 // const userSchema = require("./models/user");
 app.use((req, res) => {
     res.json({ message: "Requête reçue !" });
 });
-
-/**DOTENV ***********************************************************/
-const dotenv = require("dotenv");
-dotenv.config();
-
-const MY_PORT = process.env.PORT;
-const MY_APP_SECRET = process.env.APP_SECRET;
-
-app.get("/", (req, res) => {
-    return res.send(MY_APP_SECRET);
-});
-
-app.listen(MY_PORT, () => console.log(`Server running on port ${MY_PORT}`));
-/**FIN DOTENV ***********************************************************/
-
-module.exports = app;
-
-// /**Fonction de gestion des erreurs du serveur - elle les recherche et les gère de manière appropriée*/
-// const errorHandler = error => {
-//     if (error.syscall !== "listen") {
-//         throw error;
-//     }
-//     const address = server.address();
-//     const bind = typeof address === "string" ? "pipe" + address : "port:" + port;
-//     switch (error.code) {
-//         case "EACCES":
-//             console.error(bind + " requires elevated privileges.");
-//             process.exit(1);
-//             break;
-//         case "EADDRINUSE":
-//             console.error(bind + " is already in use.");
-//             process.exit(1);
-//             break;
-//         default:
-//             throw error;
-//     }
-// };
-// const server = http.createServer(app);
-
-// server.on("error", errorHandler);
-// server.on("listening", () => {
-//     const address = server.address();
-//     const bind = typeof address === "string" ? "pipe" + address : "port " + port;
-//     console.log("Listening on " + bind);
-// });
-// server.listen(port);
