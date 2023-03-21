98913502e5f502119f157a7fdb5f397b4bcaa865
Eline31 <eline.chambon@gmail.com>
2023-03-21 16:41:23 +0100

    update delete sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -88,7 +88,7 @@ exports.deleteSauce = (req, res, next) => {
                 const filename = sauce.imageUrl.split('/images/')[1];
                 fs.unlink(`images/${filename}`, () => {
                     Sauce.deleteOne({ _id: req.params.id })
-                        .then(() => { res.status(200).json({ message: "Votre sauce a bien été supprimée !" }) })
+                        .then(() => res.status(200).json({ message: "Votre sauce a bien été supprimée !" }))
                         .catch((error) => res.status(400).json({ error }));
                 });
             }
