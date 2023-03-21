1d6d69d45d5037746f2ad421414a6d41404e4996
Eline31 <eline.chambon@gmail.com>
2023-03-21 19:17:34 +0100

    Revert "EmailValidator OK, password validator pas ok"
    This reverts commit bd2942293a4a0f0db766f80817d485038f09dfc0.

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const emailValidator = require("email-validator");
-const passwordValidator = require("password-validator");
 // const uniqueValidator = require("mongoose-unique-validator");
 
 /**Création d'un schéma de données contenant les champs souhaités */
@@ -9,18 +8,7 @@ const userSchema = mongoose.Schema({
     password: { type: String, required: true },
 });
 
-const schemaPassword = new passwordValidator();
-schemaPassword
-    .is().min(8)                                    // Minimum length 8
-    .is().max(100)                                  // Maximum length 100
-    .has().uppercase()                              // Must have uppercase letters
-    .has().lowercase()                              // Must have lowercase letters
-    .has().digits(2)                                // Must have at least 2 digits
-    .has().not().spaces()                           // Should not have spaces
-    .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
-
 emailValidator.validate(userSchema.email);
-schemaPassword.validate(userSchema.password);
 
 module.exports = mongoose.model("User", userSchema);
 
