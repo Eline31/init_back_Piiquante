6111a94b7d1a3c3cdaeba5779e1fcab30dd05fa6
Eline31 <eline.chambon@gmail.com>
2023-03-22 11:23:03 +0100

    Like frontend OK mais pas sur la BdD mongoDB

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -36,26 +36,53 @@ exports.createSauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
+// /**Enregistrement des likes et dislikes des utilisateurs pour une sauce */
+// exports.likeSauce = (req, res, next) => {
+//     Sauce.findOne({ _id: req.params.id })
+//         .then((sauce) => {
+//             if (req.body.like = 1) {
+//                 sauce.usersLiked.push(req.body.userId);
+//                 sauce.likes++;
+//                 res.status(200).json({ message: "Votre like a bien été pris en compte !" });
+//             } if (req.body.like = 0) {
+//                 sauce.usersLiked.filter((user) => user.userId !== req.body.userId);
+//                 sauce.likes--;
+//                 res.status(200).json({ message: "Votre like a bien été retiré !" });
+//             } if (req.body.like = -1) {
+//                 sauce.usersDisliked.push(req.body.userId);
+//                 sauce.dislikes++;
+//                 res.status(200).json({ message: "Votre dislike a bien été pris en compte !" });
+//             } if (req.body.dislike = 0) {
+//                 sauce.usersLiked.filter((user) => user.userId !== req.body.userId);
+//                 sauce.dislikes--;
+//                 res.status(200).json({ message: "Votre like a bien été retiré !" });
+//             };
+//         })
+//         .catch(error => res.status(404).json({ error }));
+// };
+
 /**Enregistrement des likes et dislikes des utilisateurs pour une sauce */
 exports.likeSauce = (req, res, next) => {
+    const like = req.body.like;
+    const dislike = req.body.dislike;
     Sauce.findOne({ _id: req.params.id })
         .then((sauce) => {
-            if (req.body.like = 1) {
+            if (like === 1) {
                 sauce.usersLiked.push(req.body.userId);
                 sauce.likes++;
                 res.status(200).json({ message: "Votre like a bien été pris en compte !" });
-            } if (req.body.like = 0) {
+            } if (like === 0) {
                 sauce.usersLiked.filter((user) => user.userId !== req.body.userId);
                 sauce.likes--;
                 res.status(200).json({ message: "Votre like a bien été retiré !" });
-            } if (req.body.like = -1) {
+            } if ((like === -1) || (dislike === 1)) {
                 sauce.usersDisliked.push(req.body.userId);
                 sauce.dislikes++;
                 res.status(200).json({ message: "Votre dislike a bien été pris en compte !" });
-            } if (req.body.dislike = 0) {
+            } if (dislike === 0) {
                 // sauce.usersLiked.filter((user) => user.userId !== req.body.userId);
                 sauce.usersDisliked.$pull(req.body.userId);
-                sauce.likes--;
+                sauce.dislikes--;
                 res.status(200).json({ message: "Votre like a bien été retiré !" });
             };
         })
