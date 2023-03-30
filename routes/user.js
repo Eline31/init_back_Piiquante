3e0a1953b2a1ed9b955aa92c85ab552a11465e10
Eline31 <eline.chambon@gmail.com>
2023-03-30 11:31:22 +0200

    supp commentaire inutile dans user

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,8 @@ const userCtrl = require("../controllers/user");
 const emailCtrl = require("../middleware/email_control");
 const passwordCtrl = require("../middleware/password_control");
 
+/**Les différentes routes utilisateurs et leurs middlewares */
 router.post("/signup", emailCtrl, passwordCtrl, userCtrl.signup);
 router.post("/login", userCtrl.login);
-//Middleware password à créer
 
 module.exports = router;
