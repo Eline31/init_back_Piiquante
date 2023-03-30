28da4d54a7a1f6bb20bee2728fd3e7bc4b5b5fdf
Eline31 <eline.chambon@gmail.com>
2023-03-30 11:54:07 +0200

    createServer https

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
+const https = require("https");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const MY_PORT = process.env.PORT;
 
-app.listen(MY_PORT, () => console.log(`Server running on port ${MY_PORT}`));
+https.createServer(app).listen(MY_PORT, () => console.log(`Server running on port ${MY_PORT}`));
